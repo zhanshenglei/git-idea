01537bcb7557d4c4916db5133ea51caf419c9d56
zhanshenglei <2057418545@qq.com>
2021-08-24 08:53:52 +0800

    添加到git并修改

---
+++ maven-ssm-demo02/src/main/java/com/gittest.java
@@ -0,0 +1,15 @@
+package com;
+
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+    public void list(){
+        int i=0;
+        i+=10;
+    }
+}
